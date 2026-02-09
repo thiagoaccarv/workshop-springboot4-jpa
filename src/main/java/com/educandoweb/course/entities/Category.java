064841e1e6ae9fb7c1fab099750bb18a38965644
Thiago Carvalho <thiagoaccarv@gmail.com>
2026-02-08 21:17:57 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -20,7 +20,8 @@ public class Category implements Serializable {
     public Category() {
     }
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category(Long id, String name) {
